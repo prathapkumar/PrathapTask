224c08f6d475f5545ea62566f9a49c57040eaa15
test <test@srinuu.local>
2016-07-25 09:51:54 +0530

    Initial Commit

---
+++ PrathapTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PrathapTask
+//
+//  Created by test on 7/25/16.
+//  Copyright © 2016 Wipro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PrathapTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrathapTask
+//
+//  Created by test on 7/25/16.
+//  Copyright © 2016 Wipro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
